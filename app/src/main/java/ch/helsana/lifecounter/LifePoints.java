f4abd72b5582a6ec0512d9eaa4d1f557482915b4
DRay6 <dray6tea@gmail.com>
2021-01-14 09:16:35 +0100

    Create class LifePoints

---
+++ app/src/main/java/ch/helsana/lifecounter/LifePoints.java
@@ -0,0 +1,34 @@
+package ch.helsana.lifecounter;
+
+public class LifePoints {
+
+    private static LifePoints instance;
+    private int lp;
+    private static final int startLp = 8000;
+
+    private LifePoints() {
+        lp = startLp;
+    }
+
+    public static LifePoints getSingletonInstance(){
+        if (instance != null){
+            instance = new LifePoints();
+        }
+
+        return instance;
+    }
+
+    public void resetLifePoints(){
+        lp = startLp;
+    }
+
+    public void modifyLifePoints(int i){
+        if (i % 100 == 0){
+            lp = lp + i;
+        }
+    }
+
+    public int getLp() {
+        return lp;
+    }
+}
