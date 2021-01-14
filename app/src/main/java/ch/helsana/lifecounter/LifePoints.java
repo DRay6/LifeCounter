4133c0950dd8934281e03afa731c309dbc0b48fc
DRay6 <dray6tea@gmail.com>
2021-01-14 15:50:58 +0100

    Finish LifeCounter-Activity

--- app/src/main/java/ch/helsana/lifecounter/LifePoints.java
+++ app/src/main/java/ch/helsana/lifecounter/LifePoints.java
@@ -11,7 +11,7 @@ public class LifePoints {
     }
 
     public static LifePoints getSingletonInstance(){
-        if (instance != null){
+        if (instance == null){
             instance = new LifePoints();
         }
 
--- app/src/main/java/ch/helsana/lifecounter/MainActivity.java
+++ app/src/main/java/ch/helsana/lifecounter/MainActivity.java
@@ -4,20 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlus100;
+    Button btnPlus500;
+    Button btnPlus1000;
+    Button btnMinus100;
+    Button btnMinus500;
+    Button btnMinus1000;
+    Button btReset;
+    TextView textView;
+    LifePoints lifePoints;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btPlus100 =  findViewById(R.id.btPlus100);
-        Button btPlus500 =  findViewById(R.id.btPlus500);
-        Button btPlus1000 = findViewById(R.id.btPlus1000);
-        Button btMinus100 =  findViewById(R.id.btMin100);
-        Button btMinus500 =  findViewById(R.id.btMin500);
-        Button btMinus1000 = findViewById(R.id.btMin1000);
+        btnPlus100 = findViewById(R.id.btPlus100);
+        btnPlus500 = findViewById(R.id.btPlus500);
+        btnPlus1000 = findViewById(R.id.btPlus1000);
+        btnMinus100 = findViewById(R.id.btMin100);
+        btnMinus500 = findViewById(R.id.btMin500);
+        btnMinus1000 = findViewById(R.id.btMin1000);
+
+        btReset = findViewById(R.id.btReset);
+        textView = findViewById(R.id.activity_lifePoints_text);
+        progressBar = findViewById(R.id.progressBar);
+
+        lifePoints = LifePoints.getSingletonInstance();
+
+        updateLifePoints();
+
+        btnPlus100.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(100);
+            updateLifePoints();
+
+        });
+
+        btnPlus500.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(500);
+            updateLifePoints();
+
+        });
+
+        btnPlus1000.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(1000);
+            updateLifePoints();
+        });
+
+        btnMinus100.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(-100);
+            updateLifePoints();
+        });
+
+        btnMinus500.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(-500);
+            updateLifePoints();
+        });
+
+        btnMinus1000.setOnClickListener(btn -> {
+            lifePoints.modifyLifePoints(-1000);
+            updateLifePoints();
+        });
+
+        btReset.setOnClickListener(btn -> {
+            lifePoints.resetLifePoints();
+            updateLifePoints();
+        });
+
+
+    }
+
+    private void updateLifePoints(){
+        textView.setText(String.valueOf(lifePoints.getLp()));
+        progressBar.setProgress(lifePoints.getLp());
 
     }
 }
