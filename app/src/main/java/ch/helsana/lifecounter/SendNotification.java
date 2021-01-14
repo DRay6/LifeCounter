cbeeac5bc2118145012b3d2c9a4a8c67a17b0507
DRay6 <dray6tea@gmail.com>
2021-01-14 16:37:24 +0100

    Create NotificationChannel in NotificationSender

--- app/src/main/java/ch/helsana/lifecounter/SendNotification.java
+++ app/src/main/java/ch/helsana/lifecounter/SendNotification.java
@@ -1,8 +1,10 @@
 package ch.helsana.lifecounter;
 
+import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -15,41 +17,39 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-public class SendNotification extends Service {
-    private NotificationManager notificationManager;
-    private NotificationCompat.Builder builder;
-    private IBinder binder;
-    private Context context;
+public class SendNotification extends IntentService {
+    private static final int NOTIFICATION_ID = 3;
 
+    public SendNotification() {
+        super("SendNotification");
+    }
 
-    @Nullable
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(R.string.channel_id, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,);
+        builder.setContentTitle("YuGiOh LifeCounter");
+        builder.setContentText("Check the newest banlist");
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //to be able to launch your activity from the notification
+        builder.setContentIntent(pendingIntent);
+        Notification notificationCompat = builder.build();
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFICATION_ID, notificationCompat);
     }
-
-
-
-/*    @Override
-    protected void onCreate() {
-        super.onCreate();
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.notify);
-        button.setOnClickListener(v -> sendNotification());
-
-        this.notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private void sendNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(android.R.drawable.ic_media_play)
-                .setContentTitle("Snow")
-                .setContentText("It's snowing!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        notificationManager.notify(0, builder.build());
-    }*/
 }
