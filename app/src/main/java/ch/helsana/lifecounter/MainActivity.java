eb55dcaed79af8fbf694190ddab4c5a6d9c1f6ce
DRay6 <dray6tea@gmail.com>
2021-01-14 15:13:02 +0100

    Design activity_main.xml

--- app/src/main/java/ch/helsana/lifecounter/MainActivity.java
+++ app/src/main/java/ch/helsana/lifecounter/MainActivity.java
@@ -3,6 +3,7 @@ package ch.helsana.lifecounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btPlus100 =  findViewById(R.id.btPlus100);
+        Button btPlus500 =  findViewById(R.id.btPlus500);
+        Button btPlus1000 = findViewById(R.id.btPlus1000);
+        Button btMinus100 =  findViewById(R.id.btMin100);
+        Button btMinus500 =  findViewById(R.id.btMin500);
+        Button btMinus1000 = findViewById(R.id.btMin1000);
 
     }
 }
