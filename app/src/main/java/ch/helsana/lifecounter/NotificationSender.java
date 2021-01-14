76b502ff5451edd1e1b1d49b74fad35248f807b7
DRay6 <dray6tea@gmail.com>
2021-01-14 16:37:41 +0100

    Create Notification Intent in NotificationSender

---
+++ app/src/main/java/ch/helsana/lifecounter/NotificationSender.java
@@ -0,0 +1,13 @@
+package ch.helsana.lifecounter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationSender extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent intent1 = new Intent(context, SendNotification.class);
+        context.startService(intent1);
+    }
+}
